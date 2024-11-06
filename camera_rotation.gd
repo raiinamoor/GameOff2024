b8ff39fee2aad77c7bacbd0062e03191cb8cee85
Raii <143903432+raiinamoor@users.noreply.github.com>
2024-11-06 23:17:57 +0300

    Set up ray cast to cursor
    The RayCast3D will be used for interactions with 3D objects (characters, the pinboard and such) using the cursor

--- camera_rotation.gd
+++ camera_rotation.gd
@@ -3,7 +3,7 @@ extends Camera3D
 
 @export var horizontal_limit_deg: int = 90
 @export var vertical_limit_deg: int = 90
-@export var lerp_coefficient: float = 5
+@export var smoothing_coefficient: float = 5
 
 var target_rotation: Vector3 = Vector3.ZERO
 
@@ -12,12 +12,19 @@ var target_rotation: Vector3 = Vector3.ZERO
 	get: return viewport.get_visible_rect().get_center()
 @onready var initial_rotation = rotation_degrees
 
+@onready var cursor_ray := $RayCast3D
+
 
 func _process(delta: float) -> void:
-	var centered_mouse_pos = (viewport.get_mouse_position() - viewport_center)
+	var mouse_pos = viewport.get_mouse_position()
+	var centered_mouse_pos = mouse_pos - viewport_center
 	centered_mouse_pos = centered_mouse_pos.clamp(-viewport_center, viewport_center)
 	target_rotation.y = initial_rotation.y - lerp(0, horizontal_limit_deg, centered_mouse_pos.x / viewport_center.x)
 	target_rotation.x = initial_rotation.x - lerp(0, vertical_limit_deg, centered_mouse_pos.y / viewport_center.y)
 	
-	rotation_degrees = lerp(rotation_degrees, target_rotation, lerp_coefficient * delta)
+	# point the ray to where the cursor is
+	var ray_end = project_ray_normal(mouse_pos) * 1000
+	cursor_ray.target_position = ray_end
+	
+	rotation_degrees = lerp(rotation_degrees, target_rotation, smoothing_coefficient * delta)
 	
