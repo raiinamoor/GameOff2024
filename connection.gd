14f119354ff228be78996c021f5c89b6554ab062
Raii <143903432+raiinamoor@users.noreply.github.com>
2024-11-09 00:19:33 +0300

    Implement movable notes and connections
    - You can move notes on the screen (made using buttons)
    - You can connect notes by hovering over a note, pressing one of the buttons and then clicking another note
    - Cancel the connection by right clicking while connecting
    - Remove an existing connection by right clicking it

---
+++ connection.gd
@@ -0,0 +1,50 @@
+class_name Connection
+extends Area2D
+
+
+signal connection_removed(connection: Connection)
+
+var end_node: Button = null
+
+@onready var root_node: Button = get_parent()
+@onready var line_2d: Line2D = $Line2D
+@onready var collider: CollisionShape2D = $Collider
+
+
+func _ready() -> void:
+	line_2d.points = [Vector2.ZERO, Vector2.ZERO]
+
+
+func _process(delta: float) -> void:
+	# calculate end position of Line2D
+	if end_node == null:
+		line_2d.points[-1] = get_local_mouse_position()
+	else:
+		var end_position: Vector2 = end_node.get_rect().get_center()
+		# TODO make it so that the line connects to the edge of the note
+		#var position_on_edge: Vector2 = (end_position - root_node.position).clamp(-end_node.get_rect().size / 2, end_node.get_rect().size / 2)
+		#line_2d.points[-1] = to_local( end_position - position_on_edge )
+		line_2d.points[-1] = to_local( end_position )
+	
+	#position CollisionShape2D in the middle of the line and rotate it
+	collider.position = lerp(line_2d.points[0], line_2d.points[1], 0.5)
+	collider.shape.size.x = (line_2d.points[0] - line_2d.points[1]).length()
+	collider.rotation = line_2d.points[0].angle_to_point(line_2d.points[1])
+
+
+func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
+	if event.is_action_pressed("remove_connection"):
+		remove_connection()
+
+
+func connect_to(target: Button) -> void:
+	end_node = target
+	root_node.connections[target] = self
+	collider.disabled = false
+	connection_removed.connect(root_node.remove_connection_from_list)
+	print("Connections from {name}: {cons}".format({"name": root_node.name, "cons": root_node.connections}))
+
+
+func remove_connection() -> void:
+	connection_removed.emit(self)
+	queue_free()
---
+++ note.gd
@@ -0,0 +1,80 @@
+extends Button
+
+
+enum { 
+	CURSOR_DEFAULT,
+	CURSOR_CONNECTING
+ }
+
+@export var connection_scn: PackedScene
+var connections: Dictionary = {}
+var is_picked: bool = false
+var offset_from_mouse: Vector2 = Vector2.ZERO
+static var cursor_state = CURSOR_DEFAULT
+static var curr_connection: Node2D
+
+
+func _process(delta: float) -> void:
+	if is_picked:
+		position = get_global_mouse_position() + offset_from_mouse
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("cancel_connection") and curr_connection != null:
+		cancel_connection()
+
+
+func init_connection() -> void:
+	if not cursor_state == CURSOR_DEFAULT:
+		return
+	
+	var connection := connection_scn.instantiate()
+	connection.position = get_rect().get_center() - position
+	add_child(connection)
+	
+	curr_connection = connection
+	cursor_state = CURSOR_CONNECTING
+
+
+func complete_connection() -> void:
+	# TODO break this if statement down maybe?
+	if cursor_state == CURSOR_CONNECTING \
+			and curr_connection.get_parent() != self \
+			and curr_connection.get_parent().connections.get(self) == null \
+			and connections.get(curr_connection.get_parent()) == null:
+		curr_connection.connect_to(self)
+		curr_connection = null
+		cursor_state = CURSOR_DEFAULT
+
+
+func cancel_connection() -> void:
+	curr_connection.queue_free()
+	curr_connection = null
+	cursor_state = CURSOR_DEFAULT
+
+
+func remove_connection_from_list(connection: Connection) -> void:
+	connections.erase(connections.find_key(connection))
+
+
+func pick_up() -> void:
+	is_picked = true
+	offset_from_mouse = position - get_global_mouse_position()
+
+
+func put_down() -> void:
+	is_picked = false
+
+
+func show_buttons() -> void:
+	#$AnimationPlayer.play("show_connection_buttons")
+	$ButtonPanel.visible = true
+	var t: Tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	t.set_parallel()
+	t.tween_property($ButtonPanel, "position:y", -100, 0.5)
+
+
+func hide_buttons() -> void:
+	var t: Tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	t.set_parallel()
+	t.tween_property($ButtonPanel, "position:y", 0, 0.5)
