95896054f0f52a6bdd62f7a164c6fedb5a579c5d
Raii <143903432+raiinamoor@users.noreply.github.com>
2024-11-25 16:13:55 +0300

    Changed connection scene's type from Area2D to Control
    The way input events are consumed by Controls makes it impossible to use mouse inputs with Node2Ds, so connection has to be a Control

--- Pinboard/Connections/connection.gd
+++ Pinboard/Connections/connection.gd
@@ -1,5 +1,5 @@
 class_name Connection
-extends Area2D
+extends Control
 
 
 signal connection_removed(connection: Connection)
@@ -9,7 +9,7 @@ var end_node: Button = null
 
 @onready var root_node: Button = get_parent()
 @onready var line_2d: Line2D = $Line2D
-@onready var collider: CollisionShape2D = $Collider
+@onready var button: Button = $Button
 
 
 func _ready() -> void:
@@ -25,29 +25,31 @@ func _process(delta: float) -> void:
 		# TODO make it so that the line connects to the edge of the note
 		#var position_on_edge: Vector2 = (end_position - root_node.global_position).clamp(-end_node.get_rect().size / 2, end_node.get_rect().size / 2)
 		#line_2d.points[-1] = to_local( end_position - position_on_edge )
-		line_2d.points[-1] = to_local( end_position )
+		line_2d.points[-1] = end_position - global_position
 	
 	#position CollisionShape2D in the middle of the line and rotate it
-	collider.position = lerp(line_2d.points[0], line_2d.points[1], 0.5)
-	collider.shape.size.x = (line_2d.points[0] - line_2d.points[1]).length()
-	collider.rotation = line_2d.points[0].angle_to_point(line_2d.points[1])
+	#collider.position = lerp(line_2d.points[0], line_2d.points[1], 0.5)
+	#collider.shape.size.x = (line_2d.points[0] - line_2d.points[1]).length()
+	#collider.rotation = line_2d.points[0].angle_to_point(line_2d.points[1])
+	button.position = line_2d.points[0]
+	button.size.x = (line_2d.points[0] - line_2d.points[1]).length()
+	button.rotation = line_2d.points[0].angle_to_point(line_2d.points[1])
 
 
-func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
-	if event.is_action_pressed("remove_connection"):
-		remove_connection()
+#func _input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
+	#if event.is_action_pressed("remove_connection"):
+		#remove_connection()
 
 
 func connect_to(target: Button) -> void:
 	end_node = target
 	root_node.connections[target] = self
-	collider.disabled = false
+	button.disabled = false
 	connection_removed.connect(root_node.remove_connection_from_list)
 	print("Connections from {name}: {cons}".format({"name": root_node.name, "cons": root_node.connections}))
 
 
 func remove_connection() -> void:
-	collider.disabled = true
 	var move_back_tween: Tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
 	move_back_tween.tween_property(line_2d, "points", PackedVector2Array([line_2d.points[0], line_2d.points[0]]), 1) 
 	
@@ -57,4 +59,4 @@ func remove_connection() -> void:
 
 
 func lock() -> void:
-	collider.disabled = true
+	button.disabled = true
