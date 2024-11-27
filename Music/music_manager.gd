9de7927b51bce4aa142108578da54ccf1e436c3f
Raii <143903432+raiinamoor@users.noreply.github.com>
2024-11-27 18:56:50 +0300

    Add title theme and update music manager

--- Music/music_manager.gd
+++ Music/music_manager.gd
@@ -1,30 +1,28 @@
 extends Node
 
 
-var min_volume = ProjectSettings.get_setting("audio/buses/channel_disable_threshold_db")
-
 @export var starting_track: NodePath
 @export var transition_duration: float = 3
+var min_volume = ProjectSettings.get_setting("audio/buses/channel_disable_threshold_db")
+
+@onready var curr_track: AudioStreamPlayer = $TitleTheme:
+	set(value):
+		transition(value)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+#func _ready() -> void:
+	#play()
+#
+#
+#func play(what: AudioStreamPlayer):
+	#for child in get_children():
+		#child.volume_db = -60
+	#what.volume_db = 0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_down"):
-		transition($"1", $"2")
-	if event.is_action_pressed("ui_up"):
-		transition($"2", $"1")
-
-
-func transition(from: AudioStreamPlayer, to: AudioStreamPlayer):
+func transition(to: AudioStreamPlayer):
 	var volume_tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC).set_parallel()
-	volume_tween.tween_property(from, "volume_db", min_volume, transition_duration)
+	volume_tween.tween_property(curr_track, "volume_db", min_volume, transition_duration)
 	volume_tween.tween_property(to, "volume_db", 0, transition_duration)
+	await volume_tween.finished
+	curr_track = to
