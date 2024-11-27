dbe4d16b92c06e9752bc46bd0c47652114757bad
Raii <143903432+raiinamoor@users.noreply.github.com>
2024-11-27 18:50:20 +0300

    Add preloading for character timelines

--- Characters/character.gd
+++ Characters/character.gd
@@ -2,8 +2,16 @@ extends Area3D
 
 
 @export var timeline_name: String = ""
+static var mitzie_style: DialogicStyle = load("res://Characters/Dialogue/mitzie_dialogue_style.tres").prepare()
+static var cain_style: DialogicStyle = load("res://Characters/Dialogue/cain_dialogue_style.tres").prepare()
+static var aunt_j_style: DialogicStyle = load("res://Characters/Dialogue/aunt_j_dialogue_style.tres").prepare()
+var timeline
 
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+
+
+func _ready() -> void:
+	timeline = Dialogic.preload_timeline("res://Characters/Dialogue/Timelines/" + timeline_name + ".dtl")
 
 
 func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
